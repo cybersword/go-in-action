7023e209e5954d01327f09ca14e98362ace161ba
cybersword <liuleidejian@gmail.com>
2018-03-26 21:54:56 +0800

    pool

---
+++ pool/pool.go
@@ -0,0 +1,64 @@
+package pool
+
+import (
+	"errors"
+	"io"
+	"log"
+	"sync"
+)
+
+type Pool struct {
+	m         sync.Mutex
+	resources chan io.Closer
+	factory   func() (io.Closer, error)
+	closed    bool
+}
+
+var ErrPoolClosed = errors.New("Pool has been closed.")
+
+func New(fn func() (io.Closer, error), size uint) (*Pool, error) {
+	if size <= 0 {
+		return nil, errors.New("Size value too small.")
+	}
+
+	return &Pool{
+		factory:	fn,
+		resources:	make(chan io.Closer, size),
+	}, nil
+}
+
+func (p *Pool) Acquire() (io.Closer, error) {
+	select {
+	case r, ok := <-p.resources:
+		log.Println("Acquire:", "Shared Resource")
+		if !ok {
+			return nil, ErrPoolClosed
+		}
+		return r, nil
+	default:
+		log.Println("Acquire:", "New Resource")
+		return p.factory()
+	}
+}
+
+// Release places a new resource onto the pool.
+func (p *Pool) Release(r io.Closer) {
+	p.m.Lock()
+	defer p.m.Unlock()
+
+	if p.closed {
+		r.Close()
+		return
+	}
+
+	select {
+	// Attempt to place the new resource on the queue.
+	case p.resources <- r:
+		log.Println("Release:", "In Queue")
+	// If the queue is already at cap we close the resource.
+	default:
+		log.Println("Release:", "Closing")
+		r.Close()
+	}
+}
+
